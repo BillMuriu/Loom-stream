588d8c4385a581ee62916e6dc298da6282208e4e
unknown <njorogekamau63@gmail.com>
2025-01-28 12:20:10 +0300

    React Query Workspace provider works

---
+++ src/app/dashboard/[workspaceId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Workspace = (props: Props) => {
+  return <div>Workspace</div>;
+};
+
+export default Workspace;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import ReactQueryProvider from "@/react-query";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,7 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${manrope.className} bg-[#171717] text-white`}>
-          {children}
+          <ReactQueryProvider>{children}</ReactQueryProvider>
         </body>
       </html>
     </ClerkProvider>
